3ba79d253d782671f73cb4feed3c1da1834d2b39
Usama Ahmed Siddiquie <220usamaahmed@gmail.com>
2021-10-29 22:00:49 +0500

    Implemented ALU

--- Arithmetic.v
+++ Arithmetic.v
@@ -49,3 +49,48 @@ module INC16 (
     ADD16 add16(.sum(Y), .A(A), .B(16'h0001));
 
 endmodule
+
+module ALU (
+    output [15:0] out,
+    output zr, ng,
+    input [15:0] A, B,
+    input za, na, zb, nb, f, no
+);
+
+    wire [15:0] za_out, na_out, zb_out, nb_out;
+    wire not_za, not_zb;
+
+    NOT not_1(not_za, za);
+    NOT not_2(not_zb, zb);
+
+    genvar i;
+    generate
+        for (i = 0; i < 16; i = i + 1) begin
+            AND and_gate_1(za_out[i], not_za, A[i]);
+            AND and_gate_2(zb_out[i], not_zb, B[i]);
+
+            XOR xor_gate_1(na_out[i], na, za_out[i]);
+            XOR xor_gate_2(nb_out[i], nb, zb_out[i]);
+        end
+    endgenerate
+
+    wire [15:0] bitwise_and, sum;
+
+    AND16 and16 (bitwise_and, na_out, nb_out);
+    ADD16 add16 (sum, na_out, nb_out);
+
+    wire [15:0] f_out;
+
+    MUX16 mux16 (f_out, f, bitwise_and, sum);
+
+    genvar j;
+    generate
+        for (j = 0; j < 16; j = j + 1) begin
+            XOR xor_gate_1(out[j], no, f_out[j]);
+        end
+    endgenerate
+
+    assign ng = out[15];
+    assign zr = out == 16'h0000;
+
+endmodule
